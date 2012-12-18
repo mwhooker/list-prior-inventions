7aa386d44683ba783d0e19bb0d88d51ee7b3f9cd
Matthew Hooker <mwhooker@gmail.com>
2012-12-17 18:38:06 -0800

    bugfixes.

--- find_prior_inventions.py
+++ find_prior_inventions.py
@@ -1,18 +1,27 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 
+import json
 import sys
-from github2.client import Github
+import urllib2
+from datetime import datetime
 
-github = Github()
+def fmt_dt(ds):
+    """Accepts wc3 time and prints utc date."""
+    dt =  datetime.strptime(ds, "%Y-%m-%dT%H:%M:%SZ")
+    return dt.strftime('%Y-%m-%d')
 
 
-repos = github.repos.list(sys.argv[1])
+url = "https://api.github.com/users/%s/repos?type=owner&sort=created" % sys.argv[1]
+repos = json.loads(
+    urllib2.urlopen(url).read()
+)
 
-output = [(repo.name, repo.created_at.date(), repo.url) for repo in repos]
+output = [(repo['name'], fmt_dt(repo['created_at']), repo['html_url']) for repo in repos]
 
 biggest_title = max(map(lambda x: len(x[0]), output))
 
 format_str = "%%-%ds\t%%s\t%%s" % (biggest_title)
+print format_str
 for attributes in output:
     print format_str % attributes
