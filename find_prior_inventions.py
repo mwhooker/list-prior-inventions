34aca91c5e157486820677cb309b3b9be4e7655c
Matthew Hooker <mwhooker@gmail.com>
2024-06-27 17:50:03 -0700

    update to python3

--- find_prior_inventions.py
+++ find_prior_inventions.py
@@ -1,27 +1,28 @@
-#!/usr/bin/env python
-
+#!/usr/bin/env python3
 
 import json
 import sys
-import urllib2
+import urllib.request
 from datetime import datetime
 
 def fmt_dt(ds):
     """Accepts wc3 time and prints utc date."""
-    dt =  datetime.strptime(ds, "%Y-%m-%dT%H:%M:%SZ")
+    dt = datetime.strptime(ds, "%Y-%m-%dT%H:%M:%SZ")
     return dt.strftime('%Y-%m-%d')
 
+if len(sys.argv) < 2:
+    print("Usage: script.py <github_username>")
+    sys.exit(1)
 
 url = "https://api.github.com/users/%s/repos?type=owner&sort=created" % sys.argv[1]
-repos = json.loads(
-    urllib2.urlopen(url).read()
-)
+with urllib.request.urlopen(url) as response:
+    repos = json.loads(response.read().decode())
 
 output = [(repo['name'], fmt_dt(repo['created_at']), repo['html_url']) for repo in repos]
 
 biggest_title = max(map(lambda x: len(x[0]), output))
 
 format_str = "%%-%ds\t%%s\t%%s" % (biggest_title)
-print format_str
+print(format_str % ("Repository", "Created Date", "URL"))
 for attributes in output:
-    print format_str % attributes
+    print(format_str % attributes)
